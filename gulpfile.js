7fb55076c09e7b1043eb5b5c9250599371e6e4cd
Borderliner <hajianpour.mr@gmail.com>
2016-08-30 14:32:06 +0430

    Added a Gulp automation system

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,37 @@
-var gulp  = require('gulp'),
-	gutil = require('gulp-util');
+var gulp     = require('gulp'),
+    less     = require('gulp-less'),
+    rename   = require('gulp-rename'),
+	  gutil    = require('gulp-util'),
+    path     = require('path');
 
-gulp.task('default', function() {
-	return gutil.log('Gulp is running!');
+gulp.task('less -> css', function() {
+  gulp.src(path.join('src', 'less', '**', '*.less'))
+  .pipe(less({
+    paths: [ path.join(__dirname, 'src', 'less', 'includes') ],
+    compress: false,
+  }).on('error', function(err) {
+      gutil.log('\t', gutil.colors.red('ERROR: '), 'Could not compile \"less\" files. Is \"less\" installed globally?\n');
+    }))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('less -> css.min', function() {
+  gulp.src(path.join('src', 'less', '**', '*.less'))
+  .pipe(less({
+    paths: [ path.join(__dirname, 'src', 'less', 'includes') ],
+    compress: true
+  }).on('error', function(err) {
+      gutil.log('\t', gutil.colors.red('ERROR: '), 'Could not compile \"less\" files. Is \"less\" installed globally?\n');
+    }))
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', [
+    'less -> css',
+    'less -> css.min'
+  ], function() {
+	return gutil.log('\t', gutil.colors.green('Compiled successfully!'));
 });
