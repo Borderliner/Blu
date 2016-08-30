76e49e1f07b19d2d13defff5c96e519d0b3c6078
Borderliner <hajianpour.mr@gmail.com>
2016-08-30 22:00:35 +0430

    Fix gulpfile to be standard-compatible

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,39 @@
-var gulp     = require('gulp'),
-    less     = require('gulp-less'),
-    rename   = require('gulp-rename'),
-	  gutil    = require('gulp-util'),
-    path     = require('path');
+var gulp = require('gulp')
+var less = require('gulp-less')
+var rename = require('gulp-rename')
+var gutil = require('gulp-util')
+var path = require('path')
 
-gulp.task('less -> css', function() {
+gulp.task('less -> css', function () {
   gulp.src(path.join('src', 'less', '**', '*.less'))
   .pipe(less({
     paths: [ path.join(__dirname, 'src', 'less', 'includes') ],
-    compress: false,
-  }).on('error', function(err) {
-      gutil.log('\t', gutil.colors.red('ERROR: '), 'Could not compile \"less\" files. Is \"less\" installed globally?\n');
-    }))
-  .pipe(gulp.dest('dist'));
-});
+    compress: false
+  }).on('error', function (err) {
+    gutil.log('\t', gutil.colors.red('ERROR: '), 'Could not compile "less" files. Is "less" installed globally?\n')
+    gutil.log(err)
+  }))
+  .pipe(gulp.dest('dist'))
+})
 
-gulp.task('less -> css.min', function() {
+gulp.task('less -> css.min', function () {
   gulp.src(path.join('src', 'less', '**', '*.less'))
   .pipe(less({
     paths: [ path.join(__dirname, 'src', 'less', 'includes') ],
     compress: true
-  }).on('error', function(err) {
-      gutil.log('\t', gutil.colors.red('ERROR: '), 'Could not compile \"less\" files. Is \"less\" installed globally?\n');
-    }))
+  }).on('error', function (err) {
+    gutil.log('\t', gutil.colors.red('ERROR: '), 'Could not compile "less" files. Is "less" installed globally?\n')
+    gutil.log(err)
+  }))
   .pipe(rename({
     suffix: '.min'
   }))
-  .pipe(gulp.dest('dist'));
-});
+  .pipe(gulp.dest('dist'))
+})
 
 gulp.task('default', [
-    'less -> css',
-    'less -> css.min'
-  ], function() {
-	return gutil.log('\t', gutil.colors.green('Compiled successfully!'));
-});
+  'less -> css',
+  'less -> css.min'
+], function () {
+  return gutil.log('\t', gutil.colors.green('Compiled successfully!'))
+})
